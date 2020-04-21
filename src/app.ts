3888ab63e960b4a0f4e34fbccc79e657a6fbe4f0
unknown <aguslucero@hotmail.com>
2020-04-21 17:54:10 -0500

    mongodb atlas

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import indexRoutes from './routes/index'
 import path from 'path';
+
 var cors = require('cors')
 
 const app = express();
@@ -20,5 +21,7 @@ app.use('/api', indexRoutes);
 //carpeta para imagenes
 app.use('/uploads', express.static(path.resolve('uploads')));
 app.use('/uploads2', express.static(path.resolve('uploads2')));
+
+
 
 export default app; 
--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,12 @@
 import mongoose  from 'mongoose';
 
 export async function startConnecion() {
- await mongoose.connect('mongodb://localhost/galeria',{
+ await mongoose.connect('mongodb+srv://mongo123:mongo123@cluster0-56yrx.mongodb.net/test?retryWrites=true&w=majority',{
      useNewUrlParser: true,
      useFindAndModify:false
 
- });
-console.log('base de datos conectada');
+ })
+ .then(db => console.log('base de datos conectada'))
+ .catch(err => console.log(err))
 }
 
